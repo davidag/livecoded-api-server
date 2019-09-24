639b5a30d2efb0656fdd1882422a7243fa408126
David Alfonso <developer@davidalfonso.es>
2019-09-24 20:34:28 +0200

    video 4: creating a task and checking its details

--- test_todoserver.py
+++ test_todoserver.py
@@ -6,6 +6,12 @@ import unittest
 import json
 from todoserver import app
 
+app.testing = True
+
+
+def json_body(resp):
+    return json.loads(resp.data.decode("utf-8"))
+
 
 class TestTodoserver(unittest.TestCase):
     # better be verbose with test names, you'll only type this once!
@@ -15,5 +21,30 @@ class TestTodoserver(unittest.TestCase):
         # Expected value first (consistency in all tests)
         self.assertEqual(200, resp.status_code)
         # resp.data is of type bytes in Py3
-        data = json.loads(resp.data.decode("utf-8"))
-        self.assertEqual([], data)
+        self.assertEqual([], json_body(resp))
+
+    # create a non-trivial test that will force us to deal with several
+    # parts of the application.
+    def test_create_a_task_and_get_its_details(self):
+        client = app.test_client()
+        # verify test pre-conditions
+        resp = client.get("/tasks/")
+        self.assertEqual([], json_body(resp))
+        # create a new task
+        new_task_data = {
+            "summary": "Get milk",
+            "description": "One liter of organic milk"
+        }
+        resp = client.post("/tasks/", data=json.dumps(new_task_data))
+        self.assertEqual(201, resp.status_code)
+        data = json_body(resp)
+        self.assertIn("id", data)
+        # get task details
+        task_id = data["id"]
+        resp = client.get("/tasks/{:d}/".format(task_id))  # auto check task_id is Number
+        self.assertEqual(200, resp.status_code)
+        task = json_body(resp)
+        self.assertEqual(task_id, task["id"])
+        self.assertEqual("Get milk", task["summary"])
+        self.assertEqual("One liter of organic milk", task["description"])
+
--- todoserver.py
+++ todoserver.py
@@ -1,12 +1,37 @@
 # todoserver.py
-
-from flask import Flask, make_response
+import json
+from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
-@app.route("/tasks/")
+# Don't require a db yet, as it's not the focus of the test we're creating
+MEMORY = {}
+
+@app.route("/tasks/", methods=["GET"])
 def get_all_tasks():
     return make_response("[]", 200)
+
+
+@app.route("/tasks/", methods=["POST"])
+def create_task():
+    # request will always contain info about the current request
+    # we're not requiring the content-type header as it's not a requirement yet
+    payload = request.get_json(force=True)
+    task_id = 1
+    MEMORY[task_id] = {
+        "summary": payload["summary"],
+        "description": payload["description"],
+    }
+    task_info = {"id": task_id}
+    return make_response(json.dumps(task_info), 201)
+
+
+@app.route("/tasks/<int:task_id>/")
+def task_details(task_id):
+    task_info = MEMORY[task_id].copy()
+    task_info["id"] = task_id
+    return json.dumps(task_info)
+
 
 if __name__ == "__main__":
     app.run()
